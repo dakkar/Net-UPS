3f3387611791127eaec1709ac18587e707eed0d5
Gianni Ceccarelli <gianni.ceccarelli@net-a-porter.com>
2013-10-30 15:32:26 +0000

    offline tests

--- t/lib/Test/Net/UPS/Factory.pm
+++ t/lib/Test/Net/UPS/Factory.pm
@@ -5,10 +5,19 @@ use File::Spec;
 use Test::More;
 use Net::UPS;
 use Test::Net::UPS::NoNetwork;
+use Test::Net::UPS::Tracing;
 
 sub from_config {
     my $upsrc = File::Spec->catfile($ENV{HOME}, '.upsrc');
     return (Net::UPS->new($upsrc) or do {
+        plan(skip_all=>Net::UPS->errstr);
+        exit(0);
+    })
+}
+
+sub from_config_tracing {
+    my $upsrc = File::Spec->catfile($ENV{HOME}, '.upsrc');
+    return (Test::Net::UPS::Tracing->new($upsrc) or do {
         plan(skip_all=>Net::UPS->errstr);
         exit(0);
     })
--- t/lib/Test/Net/UPS/NoNetwork.pm
+++ t/lib/Test/Net/UPS/NoNetwork.pm
@@ -4,10 +4,11 @@ use warnings;
 use Net::UPS;
 use XML::Simple;
 use HTTP::Response;
+use Test::Deep;
 
 our @ISA=('Net::UPS');
 
-our @requests;our @responses;
+our @requests;our @testing_requests;our @responses;
 
 # we don't need this in tests, and it makes parsing the requsets
 # harder
@@ -23,41 +24,54 @@ sub post {
     );
 
     push @requests,[$url,$parsed_content];
+    if (@testing_requests) {
+        my ($url,$request,$comment) = @{shift @testing_requests};
+        cmp_deeply([$url,$parsed_content],
+                   [$url,$request],
+                   $comment || 'expected request');
+    }
 
     if (@responses) {
-        my ($status,$data) = @{shift @responses};
-        return HTTP::Response->new(
-            $status,
-            'test response',
-            [], # headers
-            XMLout(
-                $data,
-                KeepRoot => 0,
-                NoAttr => 1,
-                KeyAttr => [],
-                ForceArray => [
-                    'RatedPackage', 'RatedShipment',
-                    'AddressValidationResult',
-                ],
-            ),
-        );
+        my $data = shift @responses;
+        return (ref($data) ? ( XMLout(
+            $data,
+            KeepRoot => 1,
+            NoAttr => 1,
+            KeyAttr => [],
+            XMLDecl => 1,
+        ) ) : $data);
     }
     else {
-        return HTTP::Response->new(
-            500,
-            'no test response prepared',
-            [],
-            '',
-        );
+        die 'no test response prepared';
     }
+}
+
+sub prepare_test_from_file {
+    my ($self,$file,$comment) = @_;
+
+    my ($req_line,$request,$response) = do {
+        open my $fh,'<',$file;
+        local $/="";
+        <$fh>;
+    };
+    $req_line =~ s{^POST }{}; # remove HTTP verb, we know it's a POST
+    $request = XMLin(
+        $request,
+        KeepRoot=>1,
+        NoAttr=>1, KeyAttr=>[],
+    );
+    push @testing_requests,[$req_line,$request];
+    push @responses,$response;
+    return;
 }
 
 sub pop_last_request {
     return @{pop @requests};
 }
 
-sub push_test_response {
-    push @responses,[@_];
+sub push_test_responses {
+    shift;
+    push @responses,@_;
 }
 
 1;
