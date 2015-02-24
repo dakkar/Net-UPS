2020b19025c126039bb97ad6376755dbed371f99
Gianni Ceccarelli <gianni.ceccarelli@net-a-porter.com>
2015-02-24 12:23:49 +0000

    always 'use' IO::Socket::SSL
    under some not-very-clear circumstances, probably related to old
    versions of OpenSSL, 'require'ing IO::Socket::SSL fails while
    pre-loading the Net::SSLeay::OP_NO_* constants. 'use'ing seems to always
    work.

--- lib/Net/UPS.pm
+++ lib/Net/UPS.pm
@@ -11,6 +11,7 @@ use Net::UPS::Service;
 use Net::UPS::Address;
 use Net::UPS::Package;
 use Scalar::Util 'weaken';
+use IO::Socket::SSL;
 
 @Net::UPS::ISA          = ( "Net::UPS::ErrorHandler" );
 $Net::UPS::LIVE         = 0;
@@ -177,7 +178,6 @@ sub ssl_options {
     my ($self) = @_;
 
     unless ($self->{__args}{ssl_options}) {
-        require IO::Socket::SSL;
         require IO::Socket::SSL::Utils;
         my $cert = IO::Socket::SSL::Utils::PEM_string2cert(<<'PEM');
 -----BEGIN CERTIFICATE-----
